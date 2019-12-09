eadbfc78b03e6e9733ce2989c92fff3be59a1641
杨阳 <yangyang@JustYou.local>
2019-12-09 17:51:04 +0800

    first load

---
+++ model/layer_module.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on: 2019/5/27 14:29
+@Author: zsfeng
+"""
+
+import tensorflow as tf
+
+
+def neural_tensor_layer(class_vector, query_encoder, out_size=100):
+    """neural tensor layer (NTN)"""
+    C, H = class_vector.shape
+    # print("class_vector shape:", class_vector.shape)
+    # print("query_encoder shape:", query_encoder.shape)
+    M = tf.get_variable("M", [H, H, out_size], dtype=tf.float32,
+                        initializer=tf.keras.initializers.glorot_normal())
+    mid_pro = []
+    for slice in range(out_size):
+        slice_inter = tf.matmul(tf.matmul(class_vector, M[:, :, slice]), query_encoder, transpose_b=True)  # (C,Q)
+        mid_pro.append(slice_inter)
+    tensor_bi_product = tf.concat(mid_pro, axis=0)  # (C*K,Q)
+    # print("tensor_bi_product shape:{}".format(tensor_bi_product.shape))
+    V = tf.nn.relu(tf.transpose(tensor_bi_product))
+    W = tf.get_variable("w", [C * out_size, C], dtype=tf.float32,
+                        initializer=tf.keras.initializers.glorot_normal())
+    b = tf.get_variable("b", [C], dtype=tf.float32,
+                        initializer=tf.keras.initializers.glorot_normal())
+    probs = tf.nn.sigmoid(tf.matmul(V, W) + b)  # (Q,C)
+    return probs
+
+
+def self_attention(inputs):
+    _, sequence_length, hidden_size = inputs.shape
+    with tf.variable_scope('self_attn'):
+        x_proj = tf.layers.Dense(hidden_size)(inputs)
+        x_proj = tf.nn.tanh(x_proj)
+        u_w = tf.get_variable('W_a2', shape=[hidden_size, 1],
+                              dtype=tf.float32, initializer=tf.keras.initializers.glorot_normal())
+        x = tf.tensordot(x_proj, u_w, axes=1)
+        alphas = tf.nn.softmax(x, axis=1)
+        print("alphas shape", alphas.shape)
+        output = tf.matmul(tf.transpose(inputs, [0, 2, 1]), alphas)
+        output = tf.squeeze(output, -1)
+        return output
+
+
+def dynamic_routing(input, b_IJ, iter_routing=3):
+    ''' The routing algorithm.'''
+    '''
+    C: num of lables
+    '''
+    print("input shape",input.shape)
+    C, K, H = input.shape
+    W = tf.get_variable('W_s', shape=[H, H],
+                        dtype=tf.float32, initializer=tf.keras.initializers.glorot_normal())
+
+    for r_iter in range(iter_routing):
+        with tf.variable_scope('iter_' + str(r_iter)):
+            '''
+            将样本表征进行一次transform，使用相同的共享矩阵W
+            '''
+            d_I = tf.nn.softmax(tf.reshape(b_IJ, [C, K, 1]), axis=1)
+            # for all samples j = 1, ..., K in class i:
+            e_IJ = tf.reshape(tf.matmul(tf.reshape(input, [-1, H]), W), [C, K, -1])  # (C,K,H)
+            #对转换后的样本进行加权求和，得到初始的类别表征     C个类别，每个类别一个矩阵(C,1,H)
+            c_I = tf.reduce_sum(tf.multiply(d_I, e_IJ), axis=1, keepdims=True)  # (C,1,H)
+            c_I = tf.reshape(c_I, [C, -1])  # (C,H)
+            #将表征类别进行squash
+            c_I = squash(c_I)  # (C,H)
+            #对权重系数进行更新
+            c_produce_e = tf.matmul(e_IJ, tf.reshape(c_I, [C, H, 1]))  # (C,K,1)
+            # for all samples j = 1, ..., K in class i:
+            b_IJ += tf.reshape(c_produce_e, [C, K])
+
+    return c_I
+
+
+def squash(vector):
+    '''Squashing function corresponding to Eq. 1'''
+    vec_squared_norm = tf.reduce_sum(tf.square(vector), 1, keepdims=True)
+    scalar_factor = vec_squared_norm / (1 + vec_squared_norm) / tf.sqrt(vec_squared_norm + 1e-9)
+    vec_squashed = scalar_factor * vector  # element-wise
+    return (vec_squashed)
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    inputs = np.random.random((24, 5, 10))  # (3*3+3*5,seq_len,lstm_hidden_size*2)
+    # print (inputs)
+    inputs = tf.constant(inputs, dtype=tf.float32)
+    encoder = self_attention(inputs)  # (k*c,lstm_hidden_size*2)
+    print('encode shape',encoder.shape)
+    support_encoder = tf.slice(encoder, [0, 0], [9, 10])
+    query_encoder = tf.slice(encoder, [9, 0], [15, 10])
+
+    support_encoder = tf.reshape(support_encoder, [3, 3, -1])
+    b_IJ = tf.constant(
+        np.zeros([3, 3], dtype=np.float32))
+    class_vector = dynamic_routing(support_encoder, b_IJ)
+    inter = neural_tensor_layer(class_vector, query_encoder, out_size=10)
+
+    # test accuracy
+    query_label = [0, 1, 2] * 5
+    print(query_label)
+    predict = tf.argmax(name="predictions", input=inter, axis=1)
+    correct_prediction = tf.equal(tf.cast(predict, tf.int32), query_label)
+    accuracy = tf.reduce_mean(name="accuracy", input_tensor=tf.cast(correct_prediction, tf.float32))
+    labels_one_hot = tf.one_hot(query_label, 3, dtype=tf.float32)
+
+    sess = tf.Session()
+    with sess.as_default():
+        sess.run(tf.global_variables_initializer())
+        # print(encoder.eval())
+        # print(query_encoder.eval())
+        print(inter.eval())
+        print(predict.eval())
+        print(correct_prediction.eval())
+        print(accuracy.eval())
---
+++ util/json_loader.py
@@ -0,0 +1,92 @@
+import json
+import os
+import re
+import multiprocessing
+import numpy as np
+import random
+import gensim
+import jieba
+
+class FileDataLoader:
+    def next_batch(self, B, N, K, Q):
+        '''
+        B: batch size.
+        N: the number of relations for each batch
+        K: the number of support instances for each relation
+        Q: the number of query instances for each relation
+        return: support_set, query_set, query_label
+        '''
+        raise NotImplementedError
+
+class JSONFileDataLoader(FileDataLoader):
+    def __init__(self,path,word_vec_file_name,max_length = 15,cuda = False):
+        file_lists = self.get_file_path(path)
+        self.features = self.load_all_data(file_lists)
+        self.train_features, self.test_features = self.few_shot_feature(self.features)
+        self.word2id = {}
+        #加载词向量
+        if word_vec_file_name is None or not os.path.isfile(word_vec_file_name):
+            raise Exception("[ERROR] Word vector file doesn't exist")
+        self.word_vec = gensim.models.KeyedVectors.load_word2vec_format(word_vec_file_name)
+        self.word2id = self.word_dic()
+
+
+    def get_file_path(self,path):
+        file_lists = []
+        files = os.listdir(path)
+        for file in files:
+            if not os.path.isdir(file):
+                new_path = os.path.join(path, file)
+                if os.path.isdir(new_path):
+                    for filename in os.listdir(new_path):
+                        file_path = os.path.join(new_path, filename)
+                        file_lists.append(file_path)
+        return file_lists
+
+    def load_json_data(self,filename, features):
+        with open(filename, 'r') as f:
+            data = json.load(f)
+        for examples in data['rasa_nlu_data']['common_examples']:
+            if examples['intent'] not in features:
+                features[examples['intent']] = [examples['text']]
+            else:
+                features[examples['intent']].append(examples['text'])
+
+    def load_all_data(self,filenames):
+        features = {}
+        for filename in filenames:
+            self.load_json_data(filename, features)
+        return features
+
+    def few_shot_feature(self,features):
+        few_shot_features = {}
+        few_shot_features['机票'] = features.pop('机票')
+        few_shot_features['火车'] = features.pop('火车')
+        few_shot_features['酒店'] = features.pop('酒店')
+        return features,few_shot_features
+
+    def regex_sen(self,sen):
+        return re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+",'',sen)
+
+    def dataset_gen(self,sen,features):
+        dataset,labels = [],[]
+        for key,value in features.items():
+            for item in value:
+                dataset.append(jieba.lcut(self.regex_sen(item)))
+                labels.append(key)
+        return dataset,labels
+
+    def word_dic(self):
+        for i in range(self.word_vec.vectors.shape[0]):
+            self.word2id[self.word_vec.wv.index2word[i]] = i
+        self.word2id['UNK'] = self.word_vec.vectors.shape[0]
+        self.word2id['BLANK'] = self.word_vec.vectors.shape[0] + 1
+
+    def load_weight(self):
+        pass
+
+
+
+
+
+
