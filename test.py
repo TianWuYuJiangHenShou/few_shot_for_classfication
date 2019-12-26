9f96ad880c516dc220e15ec55abd1dd428d58f85
yangyang <1296572091@qq.com>
2019-12-26 16:00:32 +0800

    update data load ans start model unit

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 from util.json_loader import JSONFileDataLoader
+import numpy as np
 
-JSONFileDataLoader('data/train.json','/Users/yangyang/Desktop/sgns.weibo.char',prefix='train')
+train_data = JSONFileDataLoader('data/train.json','/Users/yangyang/Desktop/sgns.weibo.char',prefix='train')
+inputs,query_label = train_data.next_one_tf(5,5,5)
+print(inputs)
+
+
+# test_data = JSONFileDataLoader('data/test.json','/Users/yangyang/Desktop/sgns.weibo.char',prefix='test')
+# print(test_data.rel2scope.keys())
